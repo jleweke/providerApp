2a7a590886f2c231a6e278e1b5014b090a913bcb
Hana Worku <haworku@gmail.com>
2016-01-28 13:01:15 -0600

    Create main.js and move js logic there, wrap login and register logic

---
+++ js/main.js
@@ -0,0 +1,140 @@
+// When the document is ready
+$(document).ready(function () {
+  loginForm();
+  registerForm();
+});
+
+function loginForm(){
+    //validation rules
+  $("#login-form").validate({
+      rules: {
+          "Username": {
+              required: true
+
+          },
+          "Password": {
+              required: true
+          }
+      },
+      //perform an AJAX post to API
+      submitHandler: function (form) {
+          $.ajax({
+              type: "POST",
+              url: "https://myteamcare.org/ics.ashx",
+              data: $(form).serialize(),
+              headers: {
+                  "action": 'authenticate',
+                  "Content-type": "application/x-www-form-urlencoded",
+              },
+              success: function (message) {
+                  //check to see if it's valid JSON
+                  //try to parse the JSON
+                  //try {
+                  //    var message = JSON.parse(data);
+                  //    console.log(message);
+                  //}
+                  //catch (err) {
+                  //    alert("Contact CSF Administrator." + data);
+                  //}
+
+                  //deserialize data
+                  //var message = JSON.parse(data);
+                  ////if success exists then redirect
+                  if (message.Success) {
+                   window.location = 'memberdata.html';
+                  }
+
+                  ////else display value error
+                  else {
+                      alert(message.Error);
+                  }
+                  console.log(data);
+                  //window.location = 'memberdata.html';
+              },
+              error: function () { alert("There was an error communicating with the server.  Please try again")}
+          });
+          return false; // required to block normal submit since you used ajax
+      }
+  });
+};
+
+function registerForm(){
+    //validation rules
+$("#register-form").validate({
+      rules: {
+          "Username": {
+              required: true,
+              rangelength: [8, 30],
+              uppercase: true,
+              lowercase: true,
+              number: true
+
+          },
+          "Password": {
+              required: true
+          }
+          ,
+          "TaxId": {
+              required: true,
+              taxid: true
+          }
+          ,
+          "FirstName": {
+              required: true
+          }
+          ,
+          "LastName": {
+              required: true
+          }
+          ,
+          "Email": {
+              required: true,
+              email: true
+          }
+          ,
+          "Question1": {
+              required: true
+          }
+          ,
+          "Answer1": {
+              required: true
+          }
+          ,
+          "Question2": {
+              required: true
+          }
+          ,
+          "Answer2": {
+              required: true
+          }
+          ,
+          "Question3": {
+              required: true
+          }
+          ,
+          "Answer3": {
+              required: true
+          }
+      },
+      //perform an AJAX post to API
+      submitHandler: function (form) {
+          jQuery(form).ajaxSubmit({
+              type: "POST",
+              url: "https://myteamcare.org/ics.ashx",
+              data: $(this).serialize(),
+              headers: {
+                  "action": 'register',
+                  "Content-type": "application/x-www-form-urlencoded",
+              },
+              success: function (data) {
+                  console.log(data);
+                  alert("Registration successful. Check email for next steps");
+                  window.location = 'memberdata.html';
+              },
+              error: function () { alert("There was an error") }
+          });
+
+      }
+  });
+};
+
